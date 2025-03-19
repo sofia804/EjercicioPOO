06851dbf12ebf1aecb98671634f4e135ed47e1c8
sofia villarraga <sof.2081@gmail.com>
2025-03-19 15:01:11 -0500

    ejercicio

--- src/Perro.java
+++ src/Perro.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+class Perro extends Animal {
+    private String raza;
 
-public class Perro {
+    public Perro(String nombre, int edad, String raza) {
+        super(nombre, edad);
+        this.raza = raza;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    @Override
+    public void hacerSonido() {
+        System.out.println("El perro hace: guau guau");
+    }
 }
